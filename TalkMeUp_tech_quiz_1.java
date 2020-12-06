db538a497264a17209de8965a57e49734bffcc71
Junhyung Park <junhyung1821@gmail.com>
2020-12-05 23:04:48 -0600

    consume() and run by thread

--- TalkMeUp_tech_quiz_1.java
+++ TalkMeUp_tech_quiz_1.java
@@ -5,8 +5,33 @@ public class TalkMeUp_tech_quiz_1 {
     public static void main (String[] args) throws InterruptedException {
         final ProducerConsumer test = new ProducerConsumer();
 
-        test.produce();
+        Thread th1 = new Thread() {
+            public void run() {
+                try {
+                    test.produce();
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace(); 
+                }
+            }
+        };
 
+        Thread th2 = new Thread() {
+            public void run() {
+                try {
+                    test.consume();
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace(); 
+                }
+            }
+        };
+
+        th1.start();
+        th2.start();
+
+        th1.join();
+        th2.join();
     }
 
     public static class ProducerConsumer {
@@ -18,29 +43,47 @@ public class TalkMeUp_tech_quiz_1 {
             int value = 0;
             
             while (true) {
-                System.out.println ("Producer produced " + value);
-                Thread.sleep (1000);
+                //System.out.println ("Producer produced " + value);
                 if (q1.size() < q2.size()) {
                     if (q1.size() < q3.size()){
                         q1.add(value);
                         int temp1 = q1.peek();
-                        System.out.println ("Product " + temp1 + "is stored at Queue 1");
+                        //System.out.println ("Product " + temp1 + "is stored at Queue 1");
                     }
                     else { 
                         q3.add (value);
                         int temp3 = q3.peek();
-                        System.out.println ("Product " + temp3 + "is stored at Queue 3");
+                        //System.out.println ("Product " + temp3 + "is stored at Queue 3");
                     }
                 }
                 else {
                     q2.add (value);
                     int temp2 = q2.peek();
-                    System.out.println ("Product " + temp2 + "is stored at Queue 2");
+                    //System.out.println ("Product " + temp2 + "is stored at Queue 2");
                 }
                 value++;
 
-                notify();
+                Thread.sleep (100);
+            }
+        }
 
+        public void consume() throws InterruptedException {
+            while (true) {
+                if (q1.size() != 0) {
+                    int remVal1 = q1.remove();
+                    System.out.println ("Queue 1 Consumer consumes " + remVal1);
+                }
+    
+                if (q2.size() != 0) {
+                    int remVal2 = q2.remove();
+                    System.out.println ("Queue 2 Consumer consumes " + remVal2);
+                }
+                
+                if (q3.size() != 0) {
+                    int remVal3 = q3.remove();
+                    System.out.println ("Queue 3 Consumer consumes " + remVal3);
+                }
+    
                 Thread.sleep (1000);
             }
         }
